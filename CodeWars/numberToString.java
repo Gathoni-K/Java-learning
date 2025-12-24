be25f2949c5b26646c30a8835bd8f835585d3400
Gathoni-K <gathonikarume@gmail.com>
2025-12-24 22:38:37 +0300

    CodeWars 3

---
+++ CodeWars/numberToString.java
@@ -0,0 +1,11 @@
+/*
+A function that transforms a number to a string
+ */
+
+class Kata {
+    public static String numberToString(int num){
+        String numStr = String.valueOf(num);
+
+        return numStr;
+    }
+}
